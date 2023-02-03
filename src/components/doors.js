ceb890392e389ded665116618417f53f8e311dfd
Adam Durrant <adamdurrant92@gmail.com>
2023-02-03 22:24:17 +0000

    more setup, added single door and installed framer motion

--- src/components/doors.js
+++ src/components/doors.js
@@ -1,5 +1,16 @@
 import React from "react";
-
+import { motion } from "framer-motion";
 export default function Doors() {
-  return <div>Doors</div>;
+  return (
+    <>
+      <motion.div className='doors absolute z-10 flex'>
+        <div className='door1'>
+          <img src='images/singular-door.svg' alt='door'></img>
+        </div>
+        <div className='door2'>
+          <img src='images/singular-door.svg' alt='door'></img>
+        </div>
+      </motion.div>
+    </>
+  );
 }
--- src/components/main.js
+++ src/components/main.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Doors from "./doors";
 import Cta from "./cta";
+import { motion } from "framer-motion";
 
 export default function Main() {
   return (
     <>
       <main className='flex justify-center items-center relative'>
-        <img src='images/beach.svg' alt='beach illustration'></img>
+        <Doors />
+        <motion.img
+          src='images/beach.svg'
+          alt='beach illustration'
+        ></motion.img>
         <Cta />
       </main>
     </>
