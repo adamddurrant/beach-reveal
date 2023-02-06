17c1ea22848ddd93e2845d67d63038efb80f6f55
Adam Durrant <adamdurrant92@gmail.com>
2023-02-06 22:16:09 +0000

    swapped video player to reactplayer to build custom thumbnail

--- src/components/video.js
+++ src/components/video.js
@@ -1,33 +1,25 @@
 import React from "react";
 import { useContext } from "react";
 import { VideoContext } from "../contexts/VideoContext.js";
-import YouTube from "react-youtube";
+import ReactPlayer from "react-player/youtube";
 
 export default function Video() {
   const { video } = useContext(VideoContext);
-  const opts = {
-    height: "360",
-    width: "640",
-    playerVars: {
-      autoplay: 1,
-      loop: 1,
-      modestbranding: 1,
-    },
-  };
-  const style = {
-    borderRadius: "20px",
-  };
 
   return (
     <>
       {video && (
         <>
-          <div className='video-container rounded absolute z-50'>
-            <YouTube
-              videoId='S-A1upowdJI'
-              opts={opts}
-              className={"rounded"}
-              style={style}
+          <div className='video-container rounded absolute z-50 w-[640px] h-[360px]'>
+            <ReactPlayer
+              className='react-player'
+              url='https://www.youtube.com/watch?v=35npVaFGHMY'
+              width='100%'
+              height='100%'
+              controls={false}
+              muted={false}
+              playing={true}
+              light={<img src='images/video-thumb.svg' alt='Thumbnail' />}
             />
           </div>
         </>
